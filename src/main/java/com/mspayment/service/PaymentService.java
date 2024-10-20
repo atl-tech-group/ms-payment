4a79e871a382373f54b2885049b8ab6aa22929b6
nihadamirov <nihadamirov98@gmail.com>
2024-10-20 14:28:16 +0400

    add Transactional-propagation,isolation

--- src/main/java/com/mspayment/service/PaymentService.java
+++ src/main/java/com/mspayment/service/PaymentService.java
@@ -2,9 +2,13 @@ package com.mspayment.service;
 
 import com.mspayment.dto.request.PaymentRequestDto;
 import com.mspayment.entity.Payment;
+import com.mspayment.exceptions.MyException;
 import com.mspayment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
@@ -13,6 +17,11 @@ import java.time.LocalDate;
 public class PaymentService {
     private final PaymentRepository paymentRepository;
 
+    @Transactional(
+            propagation = Propagation.REQUIRED,
+            isolation = Isolation.DEFAULT,
+            timeout = 30,
+            rollbackFor = MyException.class)
     public void processPayment(PaymentRequestDto paymentRequestDto) {
 
         Payment payment = new Payment();
